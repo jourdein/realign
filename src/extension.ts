a71a3c04e5b76775c99c1f1f001d6c37581fac1d
Max Hillis <maxhillis@gmail.com>
2020-08-16 18:41:22 -0700

    Initial working version

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { TextDecoder } from 'util';
+import { join } from 'path';
+import { SSL_OP_MSIE_SSLV2_RSA_PADDING } from 'constants';
 
 let myStatusBarItem: vscode.StatusBarItem;
 
@@ -45,13 +48,21 @@ class AlignExt {
 	private _active: ContextKey;
 	private _bar: StatusBar;
 	private _edits: [number,string][];
+	private _doc: vscode.TextDocument|undefined;
+	private _editor: vscode.TextEditor|undefined;
+	private _selections: vscode.Selection[]|undefined;
 
 	constructor() {
 		this._active = new ContextKey('realign.active');
-		
+
 		// Create the status bar
 		this._bar = new StatusBar();
 		this._edits = [];
+
+		this._editor = undefined;
+		this._doc = undefined;
+
+		this._selections = undefined;
 	}
 
 	public is_active(): boolean{
@@ -65,9 +76,16 @@ class AlignExt {
 
 	public activate(): void{
 		console.log("Activating");
+		this._editor = vscode.window.activeTextEditor;
+		if (this._editor === undefined) {
+			console.log("No active document. Not activating");
+			return;
+		}
+		this._doc = this._editor.document;
 		this._bar.clear();
 		this._bar.show();
 		this._active.set(true);
+		this._selections = this._editor.selections;
 	}
 	
 	public deactivate(): void {
@@ -76,8 +94,13 @@ class AlignExt {
 		this._active.set(false);
 		this._bar.clear();
 		this._bar.hide();
-		let text_editor = vscode.window.activeTextEditor;
-		if (text_editor) {this.undo(text_editor);}
+
+		this._selections = undefined;
+
+		if (this._edits.length) {
+			let curr_editor = vscode.window.activeTextEditor;
+			if (curr_editor !== undefined && curr_editor.document===this._doc) {this.undo(curr_editor);}
+		}
 	}
 
 	public backspace(modifier: string): void {
@@ -91,13 +114,16 @@ class AlignExt {
 		this._bar.type(text);
 		this.align();
 	}
-	public pattern(): [Options, string] {
+	public pattern(): [Options, RegExp|undefined] {
 		// Detect options
-		let pattern = this._bar.get_text();
-		let match = pattern.match(/(.*)\/([lrc]\d+)?([lrc]\d+)?([lrc]\d+)?$/);
-		let options = new Options(match);
-		if ( match !== null ) {pattern = match[1];}
-		return [options, pattern];
+		let text = this._bar.get_text();
+		return parse_pattern(text);
+		
+		// let pattern = this._bar.get_text();
+		// let match = pattern.match(/(.*)\/([lrcf]\d+)?([lrcf]\d+)?([lrcf]\d+)?([lrcf]\d+)?$/);
+		// let options = new Options(match);
+		// if ( match !== null ) {pattern = match[1];}
+		// return [options, pattern];
 	}
 
 	public undo(text_editor: vscode.TextEditor): Thenable<boolean> {
@@ -111,108 +137,96 @@ class AlignExt {
 				console.log("  To " + line_text);
 				e.replace(text_editor.document.lineAt(line_num).range, line_text);
 			}
-		});
+		}, { undoStopBefore:false, undoStopAfter:true});
 	}
 
-	public align() {
-		let text_editor = vscode.window.activeTextEditor;
-		if (text_editor === undefined) {return;}
-		let text_doc = text_editor.document;
-		this.undo(text_editor).then( success => {
-			console.log("Successful undo");
-			this._edits = [];
-			if (!text_editor) {return;}
-			let tab_size = text_editor.options.tabSize;
-			if (tab_size === undefined || typeof tab_size === 'string') {return;}
+	public expand_lines(text_editor:vscode.TextEditor, regex:RegExp): Array<number>{
+		
+		// Expand selections where needed
+		let selections = this._selections;
+		if (selections === undefined) {return [];}
+		let all_selections = new Set<number>();
+		let doc = text_editor.document;
+		for (let idx = 0; idx < selections.length; idx++) {
+			const selection = selections[idx];
+			let upline = selection.start.line;
+			let downline = selection.end.line;
 
-			let [options, pattern] = this.pattern();
-			console.log("Aligning on pattern " + pattern);
-			if ( pattern === "" ) { return; }
-			let regex: RegExp;
-			try {
-				regex = RegExp(pattern);
-			} catch (error) {
-				console.log("Bad regex " + pattern);
-				return;
-			}
-
-			// Expand selections where needed
-			let selections = text_editor.selections;
-			let all_selections = new Set<number>();
-			for (let idx = 0; idx < selections.length; idx++) {
-				const selection = selections[idx];
-				let upline = selection.start.line;
-				let downline = selection.end.line;
-				
-				// Expand selection
-				if (selection.isEmpty || upline === downline) {
-					let lineText;
-					// Expand selection upwards
-					while (regex.test(text_doc.lineAt(upline).text)) {
-						all_selections.add(upline);
-						upline--;
-						if (upline < 0) {break;}
-					}
-					// Expand selection downwards
-					while (regex.test(text_doc.lineAt(downline).text)) {
-						all_selections.add(downline);
-						downline++;
-						if (downline >= text_doc.lineCount) {break;}
-					}
+			// Selection is all on one line, expand up and down
+			if (selection.isEmpty || upline === downline) {
+				let lineText;
+				// Expand selection upwards
+				while (regex.test(doc.lineAt(upline).text)) {
+					if (doc.lineAt(upline).isEmptyOrWhitespace) { break; }
+					all_selections.add(upline);
+					upline--;
+					if (upline < 0) { break; }
 				}
-				// Selection spans multiple lines
-				else {
-					for (let row = upline; row <= downline; row++) {
+				// Expand selection downwards
+				while (regex.test(doc.lineAt(downline).text)) {
+					if (doc.lineAt(downline).isEmptyOrWhitespace) { break; }
+					all_selections.add(downline);
+					downline++;
+					if (downline >= doc.lineCount) { break; }
+				}
+			}
+			// Selection spans multiple lines
+			else {
+				for (let row = upline; row <= downline; row++) {
+					let vsline = doc.lineAt(row);
+					if (!vsline.isEmptyOrWhitespace && regex.test(vsline.text)) {
 						all_selections.add(row);
 					}
 				}
 			}
-			// all_selections now holds the line numbers of our lines to align
-			let lines = Array.from(all_selections);
-			lines.sort();
+		}
+		// all_selections now holds the line numbers of our lines to align
+		let lines = Array.from(all_selections);
+		lines.sort();
+		return lines;
+	}
+
+	public align() {
+
+		if (this._editor === undefined) {return;}
+		if (this._doc === undefined) {return;}
+		let text_editor = this._editor;
+		let text_doc = this._doc;
+		this.undo(text_editor).then( success => {
+			console.log("Successful undo");
+			this._edits = [];
+
+			let tab_size = text_editor.options.tabSize;
+			if (tab_size === undefined || typeof tab_size === 'string') {return;}
+
+			let [options, regex] = this.pattern();
+			console.log("Aligning on pattern " + regex);
+			if ( regex === undefined ) { return; }
+			// let regex: RegExp;
+			// try {
+			// 	regex = RegExp(`(${pattern})`);
+			// } catch (error) {
+			// 	console.log("Bad regex " + pattern);
+			// 	return;
+			// }
+
+			let lines = this.expand_lines(text_editor, regex);
+			let str_lines: Array<string> = [];
+			for (const num of lines) {
+				let str_line = text_doc.lineAt(num).text;
+				str_lines.push(str_line);
+				this._edits.push([num, str_line]);
+			}
+
+			let splitter = new Splitter(str_lines, regex, options.fields, tab_size);
+			let rows = splitter.rows;
+			let wcols = splitter.col_widths;
+			let ind = splitter.indent;
+
 			console.log("Aligning lines " + (lines[0] + 1) + " to " + (lines[lines.length - 1] + 1));
+
+			let finals = splitter.to_lines(options.left, options.right);
 			
-			// Figure out who is longest
-			let row_strings = [];
-			let max_width = 0;
-			for (const num of lines) {
-				let line = text_doc.lineAt(num).text;
-				let result = regex.exec(line);
-				if (result !== null) {
-					let num_tabs = countTabs(line);
-					if (max_width < result.index+tab_size*num_tabs) {
-						max_width = result.index+tab_size*num_tabs;
-					}
-				}
-			}
-			console.log("Width is " + max_width);
-			// Loop again now putting in needed spaces
-			let finals = Array<string>();
-			for (const num of lines) {
-				let line = text_doc.lineAt(num).text;
-				this._edits.push([num, line]);
-				let split = line.split(regex);
-				let match = regex.exec(line);
-				if (match !== null) {
-					if (split !== null) {
-						let tab_size = text_editor.options.tabSize;
-						if ( tab_size === undefined ) {return;} 
-						if ( typeof tab_size === 'string' ) {return;}
-						// TODO: Handle options
-						let num_tabs = countTabs(split[0]);
-						let new_str = split[0].replace(/\t/g,' '.repeat(tab_size)).padEnd(max_width, " ");
-						if ( options.left !== -1 ) { new_str += ' '.repeat(options.left);}
-						new_str += match[0];
-						if ( options.right !== -1) { new_str += ' '.repeat(options.right);}
-						new_str += split.slice(1).join(match[0]);
-						// let new_str = split[0].replace(/\t/g,' '.repeat(tab_size)).padEnd(max_width, " ") + match[0] + split.slice(1).join(match[0]);
-						finals.push(new_str);
-					}
-				}
-				else {
-					finals.push(line);
-				}
-			}
 			// Now make our edits
 			text_editor.edit(e => {
 				for (let i = 0; i < lines.length; i++) {
@@ -223,20 +237,12 @@ class AlignExt {
 				}
 			}, { undoStopBefore: false, undoStopAfter: false });
 
-			// // Move cursor to end of last line
+			// Move cursor to end of last line
 			// let last_line = text_doc.lineAt(lines[lines.length-1]);
 			// let selection = new vscode.Selection(last_line.range.end, last_line.range.end);
 			// text_editor.selection = selection;
 		});
 	}
-}
-
-function countTabs(str:string) : number{
-	let count = 0;
-	for (const char of str) {
-		if ( char === '\t' ) {count++;}
-	}
-	return count;
 }
 
 class ContextKey {
@@ -316,13 +322,15 @@ class StatusBar {
 	}
 }
 
-class Options {
+export class Options {
 	public left: number;
 	public right: number;
+	public fields: number;
 	
 	constructor(match:Array<string>|null) {
 		this.left = -1;
 		this.right = -1;
+		this.fields = -1;
 
 		if ( match ) {
 			for (const it of match.slice(2)) {
@@ -330,15 +338,139 @@ class Options {
 				let num = +it.slice(1);
 				if (it[0] === 'l'){
 					this.left = num;
+
+					if (this.right === -1) {this.right = 1;}
 				}
 				else if (it[0] === 'r'){
 					this.right = num;
+
+					if (this.left === -1) {this.left = 1;}
 				}
 				else if (it[0] === 'c'){
 					this.left  = num;
 					this.right = num;
 				}
+				else if (it[0] === 'f'){
+					this.fields = num;
+				}
+			}
+		}
+		if ( this.left === -1 && this.right === -1 ) { this.left = 1; this.right = 1;}
+	}
+}
+
+export class Splitter {
+
+	public lines: Array<string>;
+	public regex: RegExp;
+	public maxsplit:number;
+	public tabsize:number;
+	public indent:number;
+	public rows: Array<Array<string>>;
+	public col_widths: Array<number>;
+
+	constructor(lines:Array<string>, regex:RegExp, maxsplit:number, tabsize:number){
+		this.lines = lines;
+		this.regex = regex;
+		this.tabsize = tabsize;
+		this.maxsplit = maxsplit;
+		this.indent = 9999999;
+		this.rows = [];
+		this.col_widths = [];
+
+		if (maxsplit > 0) {this.maxsplit*=2;}
+
+		this.parse_lines();
+	}
+
+	public parse_lines():void {
+		this.rows = [];
+
+		// Add all rows to table
+		for (const rawline of this.lines) {
+			let line = rawline.replace(/\t/g, ' '.repeat(this.tabsize));
+
+			// Find smallest indentation
+			let indent = line.length - line.trimLeft().length;
+			if (indent < this.indent) {this.indent = indent;}
+
+			// Removes indentation and splits on regex
+			let row = line.trimLeft().split(this.regex);
+			if ( this.maxsplit > 0 && row.length > this.maxsplit ) {
+				let newrow = row.slice(0, this.maxsplit);
+				newrow.push(row.slice(this.maxsplit).join(""));
+				row = newrow;
+			}
+
+			// Remove spaces from each split
+			for (let i = 0; i < row.length; i++) {
+				row[i] = row[i].trim();
+			}
+			this.rows.push(row);
+		}	
+		this.update_widths();
+	}
+
+	public update_widths(): void {
+		this.col_widths = [];
+		for (const row of this.rows) {
+			for (let col = 0; col < row.length; col++) {
+				let col_width = row[col].length;
+				if (this.col_widths.length-1 < col) {
+					this.col_widths.push(col_width);
+				}
+				else if (col_width > this.col_widths[col]) {
+					this.col_widths[col] = col_width;
+				}
 			}
 		}
 	}
+
+	public to_lines(left:number, right:number): Array<string> {
+		let finals = Array<string>();
+
+		for (let r = 0; r < this.rows.length; r++) {
+			let row = this.rows[r];
+			let farr :Array<string> = [" ".repeat(this.indent)];
+			for (let c = 0; c < row.length; c++) {
+				if (this.col_widths[c] === 0) {continue;}
+				let width = row[c].length;
+				
+				// Put in left options
+				if ( c !== 0 && c%2!==0 ) {
+					farr.push(" ".repeat(left));
+				}
+
+				// Put in the column
+				let pad = this.col_widths[c] - width;
+				farr.push(row[c] + " ".repeat(pad));
+
+				// Put in the right options
+				if (c%2 !== 0) {
+					farr.push(" ".repeat(right));
+				}
+			}
+			let final = farr.join("").trimRight();
+			finals.push(final);
+		}
+		return finals;
+	}
+}
+
+export function parse_pattern(text:string): [Options, RegExp|undefined] {
+	// Detect options
+	let match = text.match(/(.*)\/([lrcf]\d+)?([lrcf]\d+)?([lrcf]\d+)?([lrcf]\d+)?$/);
+	let options = new Options(match);
+	if (match !== null) { 
+		text = match[1];
+	}
+	let regex:RegExp|undefined;
+	try{
+		regex = RegExp(`(${text})`);
+	}
+	catch(error) {
+		console.log("Bad regex");
+		regex = undefined;
+	}
+	return [options, regex];
 }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,204 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as ReAlign from '../../extension';
+import { Test } from 'mocha';
+
+
+function test_bad_pattern(user_pattern:string){
+	let [opt, pattern] = ReAlign.parse_pattern(user_pattern);
+	assert.strictEqual(pattern, undefined);
+}
+function test_good_pattern(user_pattern:string, left:number, right:number, fields:number){
+	let [opt, pattern] = ReAlign.parse_pattern(user_pattern);
+	console.log(opt, pattern);
+	assert.notEqual(pattern, undefined);
+
+	assert.equal(opt.left, left);
+	assert.equal(opt.right, right);
+	assert.equal(opt.fields, fields);
+}
+function test_align(input:Array<string>, answer:Array<string>, user_pattern:string) {
+	let [opt, pattern] = ReAlign.parse_pattern(user_pattern);
+
+	if ( pattern === undefined ) { assert.notEqual(pattern, undefined); return;}
+	
+	let tabsize = 4;
+	let splitter = new ReAlign.Splitter(input, pattern, opt.fields, tabsize);
+	let relines = splitter.to_lines(opt.left, opt.right);
+	for (let i = 0; i < relines.length; i++) {
+		assert.equal(relines[i], answer[i]);
+	}
+}
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	test('Test good patterns', () => {
+
+		test_good_pattern('=', 1, 1, -1);
+		test_good_pattern('\\w+', 1, 1, -1);
+		test_good_pattern('=/l1', 1, 1, -1);
+		test_good_pattern('=/l1r2', 1, 2, -1);
+		test_good_pattern('=/c2', 2, 2, -1);
+		test_good_pattern('=/f1', 1, 1, 1);
 	});
+
+	test('Test bad patterns', () => {
+		test_bad_pattern('(=');
+		test_bad_pattern('asdf)');
+		test_bad_pattern('=/[c2');
+	});
+
+	test('Test simple', () => {
+
+		let lines = [
+			"this.lines = lines;",
+			"this.regex = regex;",
+			"this.tabsize = tabsize;",
+			"this.maxsplit = maxsplit;",
+			"this.indent = 9999999;",
+			"this.rows = [];",
+			"this.col_widths = [];"
+		];
+		let answer = [
+		"this.lines      = lines;",
+		"this.regex      = regex;",
+		"this.tabsize    = tabsize;",
+		"this.maxsplit   = maxsplit;",
+		"this.indent     = 9999999;",
+		"this.rows       = [];",
+		"this.col_widths = [];",
+		];
+
+		test_align(lines, answer, '=');
+	});
+
+	test('Test align options', () => {
+
+		let lines = [
+			"this.lines = lines;",
+			"this.regex = regex;",
+			"this.tabsize = tabsize;",
+			"this.maxsplit = maxsplit;",
+			"this.indent = 9999999;",
+			"this.rows = [];",
+			"this.col_widths = [];"
+		];
+		let answer_l2 = [
+		"this.lines       = lines;",
+		"this.regex       = regex;",
+		"this.tabsize     = tabsize;",
+		"this.maxsplit    = maxsplit;",
+		"this.indent      = 9999999;",
+		"this.rows        = [];",
+		"this.col_widths  = [];",
+		];
+
+		let answer_r0 = [
+			"this.lines      =lines;",
+			"this.regex      =regex;",
+			"this.tabsize    =tabsize;",
+			"this.maxsplit   =maxsplit;",
+			"this.indent     =9999999;",
+			"this.rows       =[];",
+			"this.col_widths =[];",
+		];
+
+		let answer_c2 = [
+			"this.lines       =  lines;",
+			"this.regex       =  regex;",
+			"this.tabsize     =  tabsize;",
+			"this.maxsplit    =  maxsplit;",
+			"this.indent      =  9999999;",
+			"this.rows        =  [];",
+			"this.col_widths  =  [];",
+		];
+
+		let answer_l0r1 = [
+			"this.lines     = lines;",
+			"this.regex     = regex;",
+			"this.tabsize   = tabsize;",
+			"this.maxsplit  = maxsplit;",
+			"this.indent    = 9999999;",
+			"this.rows      = [];",
+			"this.col_widths= [];",
+		];
+
+		test_align(lines, answer_l2, '=/l2');
+		test_align(lines, answer_r0, '=/r0');
+		test_align(lines, answer_c2, '=/c2');
+		test_align(lines, answer_l0r1, '=/l0r1');
+	});
+
+
+	test('Test fields', () => {
+
+		let lines = [
+			"disposables.push(registerCommandNice('type', function (args) { ext.type(args.text); }));",
+			"disposables.push(registerCommandNice('deleteWordLeft', function (args) { ext.backspace('word'); }));",
+			"disposables.push(registerCommandNice('deleteLeft', function (args) { ext.backspace('char'); }));",
+		];
+		
+		let answers = [
+			"disposables.push ( registerCommandNice ( 'type', function           ( args) { ext.type      ( args.text); }));",
+			"disposables.push ( registerCommandNice ( 'deleteWordLeft', function ( args) { ext.backspace ( 'word'); }));",
+			"disposables.push ( registerCommandNice ( 'deleteLeft', function     ( args) { ext.backspace ( 'char'); }));",
+		];
+		
+		let answers_l0r1 = [
+			"disposables.push( registerCommandNice( 'type', function          ( args) { ext.type     ( args.text); }));",
+			"disposables.push( registerCommandNice( 'deleteWordLeft', function( args) { ext.backspace( 'word'); }));",
+			"disposables.push( registerCommandNice( 'deleteLeft', function    ( args) { ext.backspace( 'char'); }));",
+		];
+		
+		let answers_f1 = [
+			"disposables.push ( registerCommandNice('type', function (args) { ext.type(args.text); }));",
+			"disposables.push ( registerCommandNice('deleteWordLeft', function (args) { ext.backspace('word'); }));",
+			"disposables.push ( registerCommandNice('deleteLeft', function (args) { ext.backspace('char'); }));",
+		];
+
+		let answers_f3 = [
+			"disposables.push(registerCommandNice('type', function          (args) { ext.type(args.text); }));",
+			"disposables.push(registerCommandNice('deleteWordLeft', function(args) { ext.backspace('word'); }));",
+			"disposables.push(registerCommandNice('deleteLeft', function    (args) { ext.backspace('char'); }));",
+		];
+
+
+
+		test_align(lines, answers, '\\(');
+		test_align(lines, answers_l0r1, '\\(/l0r1');
+		test_align(lines, answers_f1, '\\(/f1');
+		test_align(lines, answers_f3, '\\(/c0f3');
+		
+		
+	});
+	
+	test("Test wildcards", () => {
+
+		let lines = [
+			"// The module 'vscode' contains the VS Code extensibility API",
+			"// Import the module and reference it with the alias vscode in your code below",
+		];
+		
+		let answers = [
+		"// The     module ' vscode ' contains  the        VS  Code  extensibility  API",
+		"// Import  the      module   and       reference  it  with  the            alias  vscode  in  your  code  below",
+		];		
+
+		let answers_f1 = [
+			"// The    module 'vscode' contains the VS Code extensibility API",
+			"// Import the module and reference it with the alias vscode in your code below",
+		];
+
+		let answers_f2 = [
+			"// The     module 'vscode' contains the VS Code extensibility API",
+			"// Import  the    module and reference it with the alias vscode in your code below",
+		];
+
+		test_align(lines, answers, '\\w+');
+		test_align(lines, answers_f1, '\\w+/f1');
+		test_align(lines, answers_f2, '\\w+/f2');
+	});
+
 });
